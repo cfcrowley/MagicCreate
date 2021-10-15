fcb9abadba86f6858165c7cbb11327eefa461902
Chris Crowley <cfcrowely42@gmail.com>
2021-10-15 10:22:50 -0400

    added some models back

---
+++ MagicCreate.MVC/MagicCreate.Data/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreate.Data
+{
+    public class Card
+    {
+        [Key]
+        public int CardId { get; set; }
+        [Required]
+        [Display(Name = "Card Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Card Type")]
+        public string Type { get; set; }
+        [Required]
+        [Display(Name = "Mana Value")]
+        public int ManaValue { get; set; }
+        public int ModernId { get; set; }
+        public int LegacyId { get; set; }
+        public int CommanderId { get; set; }
+        public int StandardId { get; set; }
+    }
+}
---
+++ MagicCreate.MVC/MagicCreate.Data/Commander.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreate.Data
+{
+     public class Commander
+    {
+        [Key]
+        public int CommanderId { get; set; }
+        [Required]
+        [Display(Name = "Deck Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Card Count for Deck")]
+        public int CardCount { get; set; }
+        [Required]
+        [Display(Name = "Commander for Deck")]
+        public string General { get; set; }
+        public string DeckStyle { get; set; }
+        [Display(Name = "Average ManaCurve")]
+        public double AvgRating { get; set; }
+        public virtual List<Card> Cards { get; set; }
+    }
+}
